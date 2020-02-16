a680961158223af27396c14b67ec263ca622ed02
Mario90 <mgpinan1990@gmail.com>
2020-02-16 23:39:22 +0000

    starting the refactoring

--- test/gilder_rose.test.js
+++ test/gilder_rose.test.js
@@ -1,4 +1,12 @@
-const Item = require("../src/item");
+const { Shop, Item } = require("../src/gilded_rose");
+
+describe("Gilded Rose", function() {
+  it("should foo", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("fixme");
+  });
+});
 
 describe("Item", () => {
   beforeEach(() => {
