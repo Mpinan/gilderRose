8cdb0bf4288aadd367353aecf67bca86851ead24
Mario90 <mgpinan1990@gmail.com>
2020-02-17 00:30:19 +0000

    Extracting if-else, creating const

--- src/gilder_rose.js
+++ src/gilder_rose.js
@@ -21,7 +21,8 @@ module.exports = {
   Item,
   Shop
 };
-
+const TOP_QUALITY = 50;
+const MIN_QUALITY = 0;
 function isCheese(items) {
   items.name === "Aged Brie";
 }
@@ -34,47 +35,48 @@ function isTicket(items) {
   items.name === "Backstage passes to a TAFKAL80ETC concert";
 }
 
+function decreaseQuality(items) {
+  if (items.quality > MIN_QUALITY) {
+    items.quality -= 1;
+  }
+}
+
+function increaseQuality(items) {
+  if (items.quality < TOP_QUALITY) {
+    items.quality += 1;
+  }
+}
+
 function updateItemQuality(items) {
-  if (!isCheese(items) && !isTicket(items)) {
-    if (items.quality > 0) {
-      if (!isSulfuras(items)) {
-        items.quality -= 1;
+  if (isSulfuras(items)) {
+    return;
+  }
+  if (isCheese(items) || isTicket(items)) {
+    if (items.quality < TOP_QUALITY) {
+      increaseQuality(items);
+      if (!isTicket(items)) {
+        if (items.sellIn < 10) {
+          increaseQuality(items);
+        }
+        if (items.sellIn < 5) {
+          increaseQuality(items);
+        }
       }
     }
   } else {
-    if (items.quality < 50) {
-      items.quality = items.quality + 1;
-      if (!isTicket(items)) {
-        if (items.sellIn < 11) {
-          if (items.quality < 50) {
-            items.quality += 1;
-          }
-        }
-        if (items.sellIn < 6) {
-          if (items.quality < 50) {
-            items.quality += 1;
-          }
-        }
-      }
-    }
+    decreaseQuality(items);
   }
-  if (!isSulfuras(items)) {
-    items.sellIn -= 1;
-  }
-  if (items.sellIn < 0) {
-    if (!isCheese(items)) {
-      if (!isTicket(items)) {
-        if (items.quality > 0) {
-          if (!isSulfuras(items)) {
-            items.quality -= 1;
-          }
-        }
-      } else {
-        items.quality = items.quality - items.quality;
-      }
+
+  items.sellIn -= 1;
+
+  if (items.sellIn < MIN_QUALITY) {
+    if (isCheese(items)) {
+      increaseQuality(items);
     } else {
-      if (items.quality < 50) {
-        items.quality += 1;
+      if (isTicket(items)) {
+        items.quality -= items.quality;
+      } else {
+        decreaseQuality(items);
       }
     }
   }
